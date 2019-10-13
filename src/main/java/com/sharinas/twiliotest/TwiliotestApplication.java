253b7eea5c637c2b84e00889990228347184af11
Sharina Stubbs <sharinastubbs@gmail.com>
2019-10-13 16:43:17 -0700

    initialized Spring app

---
+++ src/main/java/com/sharinas/twiliotest/TwiliotestApplication.java
@@ -0,0 +1,13 @@
+package com.sharinas.twiliotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiliotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiliotestApplication.class, args);
+	}
+
+}
